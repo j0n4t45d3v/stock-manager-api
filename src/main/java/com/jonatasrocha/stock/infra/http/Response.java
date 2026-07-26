3e875334c36eb98554514bd237cccf3e009f5898
Jonatas <delimajonatas875@gmail.com>
2026-07-26 10:59:11 -0300

    style: remove unused imports

--- src/main/java/com/jonatasrocha/stock/infra/http/Response.java
+++ src/main/java/com/jonatasrocha/stock/infra/http/Response.java
@@ -1,7 +1,5 @@
 package com.jonatasrocha.stock.infra.http;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
